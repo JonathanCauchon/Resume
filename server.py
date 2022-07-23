2d79e6009cc48c744bf8c1ff660d1de75b32762e
Jonathan Cauchon <jnthncchn@gmail.com>
2022-07-23 01:14:10 -0400

    Add CI/CD to PythonAnywhere

--- server.py
+++ server.py
@@ -7,9 +7,13 @@ from wtforms import Form, FloatField, IntegerField, SelectField, validators
 from wtforms.fields.html5 import IntegerRangeField
 import json
 from flask import jsonify
+import git
+import hashlib
 
 
 app = Flask(__name__)
+
+GITHUB_WEBHOOKS_KEY = os.environ["PYTHONANYWHERE_KEY"]
 
 
 class cdcForm(Form):
@@ -43,6 +47,14 @@ def computeCDC(N=1000, period=320, w1=560, w2=440, kappa=45, T=300):
     return str(wvl), str(drop), str(thru), str(gd), str(bw), str(center)
 
 
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
+
+
 @ app.route("/", methods=['GET', 'POST'])
 def index():
     form = cdcForm(request.form)
@@ -70,5 +82,61 @@ def simulate():
     return jsonify(wvl=wvl, drop=drop, thru=thru, gd=gd, bw=bw, center=center)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/update-server', methods=['POST'])
+def webhook():
+    if request.method != 'POST':
+        return 'OK'
+    else:
+        abort_code = 418
+        # Do initial validations on required headers
+        if 'X-Github-Event' not in request.headers:
+            abort(abort_code)
+        if 'X-Github-Delivery' not in request.headers:
+            abort(abort_code)
+        if 'X-Hub-Signature' not in request.headers:
+            abort(abort_code)
+        if not request.is_json:
+            abort(abort_code)
+        if 'User-Agent' not in request.headers:
+            abort(abort_code)
+        ua = request.headers.get('User-Agent')
+        if not ua.startswith('GitHub-Hookshot/'):
+            abort(abort_code)
+
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'})
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"})
+
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        # webhook content type should be application/json for request.data to have the payload
+        # request.data is empty in case of x-www-form-urlencoded
+        if not is_valid_signature(x_hub_signature, request.data, GITHUB_WEBHOOKS_KEY):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(abort_code)
+
+        payload = request.get_json()
+        if payload is None:
+            print('Deploy payload is empty: {payload}'.format(
+                payload=payload))
+            abort(abort_code)
+
+        if payload['ref'] != 'refs/heads/master':
+            return json.dumps({'msg': 'Not master; ignoring'})
+
+        repo = git.Repo('https://github.com/JonathanCauchon/Resume')
+        origin = repo.remotes.origin
+
+        pull_info = origin.pull()
+
+        if len(pull_info) == 0:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+        if pull_info[0].flags > 128:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+
+        commit_hash = pull_info[0].commit.hexsha
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f'{build_commit}')
+
+        return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
